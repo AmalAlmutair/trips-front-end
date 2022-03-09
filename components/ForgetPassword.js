37631b5d1ce591a7247f38572d8c9a9c749a483b
Tahreer90 <tahreer.90z@gmail.com>
2022-03-09 17:41:29 +0300

    fixed forgetpassword

--- components/ForgetPassword.js
+++ components/ForgetPassword.js
@@ -12,12 +12,17 @@ import {
   Link,
 } from "native-base";
 
+const handleSubmit = () => {
+  alert("A new password has been into your email");
+};
+
 const ForgetPassword = ({ navigation }) => {
   return (
     <Center w="100%">
       <Box safeArea p="2" py="8" w="90%" maxW="290">
         <Heading
           size="lg"
+          marginTop="150"
           fontWeight="600"
           color="coolGray.800"
           _dark={{
@@ -26,62 +31,19 @@ const ForgetPassword = ({ navigation }) => {
         >
           Welcome
         </Heading>
-        <Heading
-          mt="1"
-          _dark={{
-            color: "warmGray.200",
-          }}
-          color="coolGray.600"
-          fontWeight="medium"
-          size="xs"
-        >
-          Sign in to continue!
-        </Heading>
 
         <VStack space={3} mt="5">
           <FormControl>
-            <FormControl.Label>Email ID</FormControl.Label>
+            <FormControl.Label>Email ID or Username</FormControl.Label>
             <Input />
           </FormControl>
           <FormControl>
-            <FormControl.Label>Password</FormControl.Label>
+            <FormControl.Label>Last Entered Password</FormControl.Label>
             <Input type="password" />
-            <Link
-              _text={{
-                fontSize: "xs",
-                fontWeight: "500",
-                color: "indigo.500",
-              }}
-              alignSelf="flex-end"
-              mt="1"
-            >
-              Forget Password?
-            </Link>
           </FormControl>
-          <Button mt="2" colorScheme="indigo">
-            Sign in
+          <Button onPress={handleSubmit} mt="2" colorScheme="indigo">
+            Submit
           </Button>
-          <HStack mt="6" justifyContent="center">
-            <Text
-              fontSize="sm"
-              color="coolGray.600"
-              _dark={{
-                color: "warmGray.200",
-              }}
-            >
-              I'm a new user.{" "}
-            </Text>
-            <Link
-              _text={{
-                color: "indigo.500",
-                fontWeight: "medium",
-                fontSize: "sm",
-              }}
-              href="#"
-            >
-              Sign Up
-            </Link>
-          </HStack>
         </VStack>
       </Box>
     </Center>
