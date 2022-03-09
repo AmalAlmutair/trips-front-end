61cc76b07f7cf2974027dfe4e17fcadc55e256af
Tahreer90 <tahreer.90z@gmail.com>
2022-03-09 19:32:45 +0300

    add home component

--- components/authentication/Signup.js
+++ components/authentication/Signup.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Alert, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import {
   Button,
   Box,
@@ -12,9 +20,9 @@ import {
 import authStore from "../../stores/authStore";
 import { values } from "mobx";
 
-const Signup = () => {
+const Signup = ({ navigation }) => {
   const [user, setUser] = useState({ username: "", password: "" });
-  const [isPasswordMatch, setIsPasswordMatch] = useState(false);
+  const [isPasswordMatch, setIsPasswordMatch] = useState(true);
   const handlePasword = (cPassword) => {
     if (user.password === cPassword) {
       setIsPasswordMatch(true);
@@ -25,71 +33,73 @@ const Signup = () => {
 
   const handleChange = () => {
     if (isPasswordMatch === true) {
-      authStore.signup(user);
+      authStore.signup(user, navigation);
     } else {
       Alert.alert("Password doen't match");
     }
   };
 
   return (
-    <Center w="100%">
-      <Box safeArea p="2" w="90%" maxW="290" py="8">
-        <Heading
-          size="lg"
-          color="coolGray.800"
-          _dark={{
-            color: "warmGray.50",
-          }}
-          fontWeight="semibold"
-        >
-          Welcome
-        </Heading>
-        <Heading
-          mt="1"
-          color="coolGray.600"
-          _dark={{
-            color: "warmGray.200",
-          }}
-          fontWeight="medium"
-          size="xs"
-        >
-          Sign up to continue!
-        </Heading>
-        <VStack space={3} mt="5">
-          <FormControl>
-            <FormControl.Label>Name</FormControl.Label>
-            <Input
-              onChangeText={(value) => {
-                setUser({ ...user, username: value });
-              }}
-            />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Password</FormControl.Label>
-            <Input
-              type="password"
-              onChangeText={(value) => {
-                setUser({ ...user, password: value });
-              }}
-            />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Confirm Password</FormControl.Label>
-            <Input
-              isInvalid={!isPasswordMatch}
-              type="password"
-              style={styles.red}
-              onChangeText={(cPassword) => {
-                handlePasword(cPassword);
-              }}
-            />
-          </FormControl>
-          <Button mt="2" colorScheme="indigo" onPress={handleChange}>
-            Sign up
-          </Button>
-        </VStack>
-      </Box>
-    </Center>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Center w="100%">
+        <Box safeArea p="2" w="90%" maxW="290" py="8">
+          <Heading
+            size="lg"
+            color="coolGray.800"
+            _dark={{
+              color: "warmGray.50",
+            }}
+            fontWeight="semibold"
+          >
+            Welcome
+          </Heading>
+          <Heading
+            mt="1"
+            color="coolGray.600"
+            _dark={{
+              color: "warmGray.200",
+            }}
+            fontWeight="medium"
+            size="xs"
+          >
+            Sign up to continue!
+          </Heading>
+          <VStack space={3} mt="5">
+            <FormControl>
+              <FormControl.Label>Name</FormControl.Label>
+              <Input
+                onChangeText={(value) => {
+                  setUser({ ...user, username: value });
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Password</FormControl.Label>
+              <Input
+                type="password"
+                onChangeText={(value) => {
+                  setUser({ ...user, password: value });
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Confirm Password</FormControl.Label>
+              <Input
+                isInvalid={!isPasswordMatch}
+                type="password"
+                style={styles.red}
+                onChangeText={(cPassword) => {
+                  handlePasword(cPassword);
+                }}
+              />
+            </FormControl>
+            <Button mt="2" colorScheme="indigo" onPress={handleChange}>
+              Sign up
+            </Button>
+          </VStack>
+        </Box>
+      </Center>
+    </TouchableWithoutFeedback>
   );
 };
 
--- stores/instance.js
+++ stores/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://192.168.150.93:8000",
+  baseURL: "http://192.168.100.220:8000",
 });
 
 export default instance;
